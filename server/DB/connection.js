e12ef65abd09b09862dade3851026d6828361ef4
pushpendrakumar <pkc3766@gmail.com>
2021-08-19 22:27:03 +0530

    added add and apply coupon endpoints

---
+++ server/DB/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+/*connects to database schedulerDB */
+module.exports.connection = function () {
+    mongoose.connect(process.env.DEVELOPMENT_MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    mongoose.connection
+      .once("open", function () {
+        console.log("Database connection has been made");
+      })
+      .on("error", function (error) {
+        console.log("error is" + error);
+      });
+  };
--- server/index.js
+++ server/index.js
@@ -1,32 +1,71 @@
-const express = require("express");
-const path = require('path');
-var bodyParser = require('body-parser')
+const express = require("express")
+const path = require('path')
 const cors = require("cors")
-const PORT = process.env.PORT || 5000;
+const DB = require("./DB/connection")
+const couponsDB = require("./DB/collections/couponsCollection")
+const PORT = process.env.PORT || 5000
+const applyCoupon = require('./utils/applyCoupon')
 
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// create application/json parser
-var jsonParser = bodyParser.json()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cors())
-app.use(jsonParser)
 
-app.get("/api", (req, res) => {
+DB.connection();
+const CouponModel = couponsDB.getCouponsCollection()
+
+app.get("/api", (res) => {
     res.json({ message: "Hello from server!" });
 });
 
 app.post('/api/apply-coupon',(req,res)=>{
+  const couponCode = req.body['couponCode']
+  const totalAmount = req.body['totalAmount']
+  CouponModel.find({'couponCode':couponCode},(err,coupons)=>{
+    if(err)
+    {
+      res.send('error occured please try again')
+    }
+    else{
+      let result = applyCoupon(coupons,totalAmount);
+      res.json(result)
+    }
+  })
+});
 
-  console.log(req.body)
-  //const couponCode = req.body['couponCode']
-  // const totalAmount = req.body['totalAmount']
-  //console.log("couponCode",couponCode)
-  // console.log("totalAmount",totalAmount)
-  res.json({'cost':1000});
+app.post('/api/add-coupon',(req,res)=>{
+  //coupon info
+  let newCoupon = {
+    'couponCode':req.body['couponCode'],
+    'createdOn':new Date(),
+    'minimumAmount':req.body['minimumAmount'],
+    'expiryDate':req.body['expiryDate'],
+    'couponType':req.body['couponType'],
+    'discount':parseFloat(req.body['discount'])
+  }
+  //update if coupon already exists else add a new coupon
+  CouponModel.updateOne({'couponCode': req.body['couponCode']},
+  {
+    $set: newCoupon
+  }
+  ,{upsert : true},(err)=>{
+      if(err)
+      {
+        console.log(err);
+        res.send(err);
+      }
+      else res.send("success")
+    }
+  );
+});
+
+app.post('api/retrieve-coupons',(req,res)=>{
+
 });
 
 app.listen(PORT, () => {
