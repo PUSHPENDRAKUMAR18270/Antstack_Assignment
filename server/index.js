ecbb71141d8197e0562c8bbdcd0b29e7553b8556
pushpendrakumar <pkc3766@gmail.com>
2021-08-21 00:46:29 +0530

    deploy error resolution

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static('../client/build'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
